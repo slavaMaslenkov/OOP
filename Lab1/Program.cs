16fc6f996450a0475aa43b485fdc64933f427712
Slava_Maslenkov <vem12@tpu.ru>
2023-12-15 00:40:05 +0700

    firstPart

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            new Person("Оля", "Семёнова", "23", Gender.female);
             Console.WriteLine("Hello, World!");
         }
     }
