533fa5b0b20eb611c5d0753d22f1f1d9db61a4e5
semolga22@gmail.com <semolga22@gmail.com>
2024-10-09 00:01:20 +0700

    (refresh):добавлены методы проверки имени и фамилии

--- ClassesLab1/Person.cs
+++ ClassesLab1/Person.cs
@@ -1,4 +1,8 @@
-﻿namespace ClassesLab1
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ClassesLab1
 {
     /// <summary>
     /// Класс Person, содержащий: имя, фамилию, возраст, пол.
@@ -9,6 +13,16 @@
         /// Имя.
         /// </summary>
         private string _name;
+
+        /// <summary>
+        /// Регулярное выражение, выявляющее русские буквы.
+        /// </summary>
+        private const string _russianRegex = @"(^[а-яА-Я]+-?[а-яА-Я]+$)";
+
+        /// <summary>
+        /// Регулярное выражение, выявляющее английские буквы.
+        /// </summary>
+        private const string _englishRegex = @"(^[a-zA-Z]+-?[a-zA-Z]+$)";
 
         /// <summary>
         /// Фамилия.
@@ -45,10 +59,122 @@
             Age = age;
             Gender = gender;
         }
-        public string Name { get; set; }
+        public string Name 
+        { 
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new NullReferenceException
+                        ("Введена пустая строка.");
+                }
+                else
+                {
+                    _name = IsCorrectName(value);
+                }
+            }
+        }
+
+        
+        public string Surname 
+        {
+            get
+            {
+                return _surname;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new NullReferenceException
+                        ("Введена пустая строка.");
+                }
+                else if (!IsSameLanguage(Name, value))
+                {
+                    throw new ArgumentOutOfRangeException
+                        ("Фамилия и имя не должны быть написаны на разных языках.");
+                }
+                else
+                {
+                    _surname = IsCorrectName(value);
+                }
+            } 
+        }
         public int Age { get; set; }
-        public string Surname { get; set; }
         public Gender Gender { get; set; }
+
+        /// <summary>
+        /// Проверяет корректность введенных данных./>.
+        /// </summary>
+        /// <returns>Корректное имя или фамилия./>.</returns>
+        public string IsCorrectName(string name)
+        {
+            string correctName;
+            TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+
+            if (name.Contains("-"))
+            {
+
+                string[] words = name.Split(new char[] { '-' });
+                if (words.Length == 2) 
+                { 
+                    if (Regex.IsMatch(words[0], _russianRegex) &&
+                    Regex.IsMatch(words[1], _russianRegex) ||
+                    (Regex.IsMatch(words[0], _englishRegex)
+                    && Regex.IsMatch(words[1], _englishRegex)))
+                    {
+                        words[0] = textInfo.ToTitleCase(words[0]);
+                        words[1] = textInfo.ToTitleCase(words[1]);
+                        correctName = string.Join("-", words);
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Составное имя и фамилия должны" +
+                            " содержать только русские или английские буквы.");
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("Некорректное значение");
+                }
+                
+            }
+
+            else if (Regex.IsMatch(name, _russianRegex) ||
+                Regex.IsMatch(name, _englishRegex))
+            {
+                correctName = textInfo.ToTitleCase(name);
+            }
+            else
+            {
+                throw new ArgumentException("Имя и фамилия должны" +
+                    "содержать только русские или английские буквы.");
+            }
+            return correctName;
+        }
+
+        /// <summary>
+        /// Проверяет написание фамилии и имени на одинаковом языке./>.
+        /// </summary>
+        /// <returns>Булевое выражение./>.</returns>
+        public Boolean IsSameLanguage(string name, string surname)
+        {
+            bool sameLanguage = false;
+
+            if (Regex.IsMatch(name, _russianRegex) &&
+                    Regex.IsMatch(surname, _russianRegex) ||
+                    (Regex.IsMatch(name, _englishRegex)
+                    && Regex.IsMatch(surname, _englishRegex)))
+            {
+                sameLanguage = true;
+            }
+
+            return sameLanguage;
+        }
 
         /// <summary>
         /// Возвращает строку с информацией об объекте/>.
