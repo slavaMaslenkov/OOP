3a4d5ff1c0d8b4a079093d41e02db60881e5e0d3
Vyacheslav <vem12@tpu.ru>
2024-10-09 19:21:17 +0700

    (Refresh): добавление классво Child Adult

--- ClassesLab1/Adult.cs
+++ ClassesLab1/Adult.cs
@@ -11,32 +11,86 @@ namespace ClassesLab1
         /// <summary>
         /// Номер паспорта.
         /// </summary>
-        private string _numberOfPassport;
+        private int _numberOfPassport;
 
         /// <summary>
         /// Серия паспорта.
         /// </summary>
-        private string _seriesOfPassport;
+        private int _seriesOfPassport;
+
+        /// <summary>
+        /// Состояние брака.
+        /// </summary>
+        private Adult _partner;
+
+        /// <summary>
+        /// Место работы.
+        /// </summary>
+        private string _job;
 
         /// <summary>
         /// Объект класса Adult по умолчанию.
         /// </summary>
-        public Adult() : this("Неизвестно", "Неизвестно", 18, Gender.Male, "0000","000000", "Холост")
+        public Adult() : this("Неизвестно", "Неизвестно", 18, Gender.Male, 0000,000000, null, null)
         { }
 
         /// <summary>
-        /// Конструктор класса Person.
+        /// Конструктор класса Adult.
         /// </summary>
         /// <param name="name">Имя.</param>
         /// <param name="surname">Фамилия.</param>
         /// <param name="age">Возраст.</param>
         /// <param name="gender">Пол.</param>
-        public Person(string name, string surname, int age, Gender gender)
+        /// <param name="numberOfPassport">Номер паспорта.</param>
+        /// <param name="seriesOfPassport">Серия паспорта.</param>
+        /// <param name="partner">Партнер по браку.</param>
+        /// <param name="job">Место работы.</param>
+        public Adult(string name, string surname, int age, Gender gender, int numberOfPassport,
+            int seriesOfPassport, Adult partner, string job) : base(name, surname, age, gender)
         {
-            Name = name;
-            Surname = surname;
-            Age = age;
-            Gender = gender;
+            NumberOfPassport = numberOfPassport;
+            SeriesOfPassport = seriesOfPassport;
+            Partner = partner;
+            Job = job;
+        }
+
+        public int NumberOfPassport { get; set; }
+        public int SeriesOfPassport { get; set; }
+
+        public Adult Partner { get; set; }
+        public string Job { get; set; }
+
+        /// <summary>
+        /// Создает рандомный объект класса Adult/>.
+        /// </summary>
+        /// <returns>Объект класса Adult/>.</returns>
+        public static Adult GetRandomPerson()
+        {
+            Random rnd = new Random();
+            string[] names = { "Катя", "Оля", "Наташа", "Света", "Галя",
+                "Слава", "Эдик", "Вова", "Даня", "Коля" };
+            string[] surnames = { "Катяшкина", "Олечкина", "Наташечкина",
+                "Светова", "Галова", "Славин", "Эдиксон", "Вовчанин", "Данон", "Коликов" };
+
+            int namesIndex = rnd.Next(names.Length - 1);
+
+            Person randomPerson = new Person();
+            randomPerson.Age = rnd.Next(Person._minAge, Person._maxAge);
+
+            if (namesIndex < names.Length * 0.5)
+            {
+                randomPerson.Name = names[namesIndex];
+                randomPerson.Surname = surnames[namesIndex];
+                randomPerson.Gender = Gender.Female;
+            }
+            else
+            {
+                randomPerson.Name = names[namesIndex];
+                randomPerson.Surname = surnames[namesIndex];
+                randomPerson.Gender = Gender.Male;
+            }
+
+            return randomPerson;
         }
     }
 }
---
+++ ClassesLab1/Child.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLab1
+{
+    internal class Child : Person
+    {
+        /// <summary>
+        /// Родитель мужчина.
+        /// </summary>
+        private Adult _parentM;
+
+        /// <summary>
+        /// Родитель женщина.
+        /// </summary>
+        private Adult _parentF;
+
+        /// <summary>
+        /// РОдитель женщина.
+        /// </summary>
+        private string _kindergarten;
+
+        /// <summary>
+        /// Объект класса Child по умолчанию.
+        /// </summary>
+        public Child() : this("Неизвестно", "Неизвестно", 18, Gender.Male, null, null, null)
+        { }
+
+        /// <summary>
+        /// Конструктор класса Child.
+        /// </summary>
+        /// <param name="name">Имя.</param>
+        /// <param name="surname">Фамилия.</param>
+        /// <param name="age">Возраст.</param>
+        /// <param name="gender">Пол.</param>
+        /// <param name="parentM">Родитель мужчина.</param>
+        /// <param name="parentF">РОдитель женщина.</param>
+        /// <param name="kindergarten">Детский сад.</param>
+        public Child(string name, string surname, int age, Gender gender, Adult parentM,
+            Adult parentF, string kindergarten) : base(name, surname, age, gender)
+        {
+            ParentM = parentM;
+            ParentF = parentF;
+            Kindergarten = kindergarten;
+        }
+
+        public Adult ParentM { get; set; }
+
+        public Adult ParentF { get; set; }
+        public string Kindergarten { get; set; }
+    }
+}
