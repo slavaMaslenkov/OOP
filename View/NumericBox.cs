508d2c7daad8a081ad20017de22a63c3294bc6f6
Vyacheslav <vem12@tpu.ru>
2024-11-23 14:20:14 +0700

    (add): new NumericBox

---
+++ View/NumericBox.cs
@@ -0,0 +1,43 @@
+﻿namespace View
+{
+    /// <summary>
+    /// Класс для ввода чисел.
+    /// </summary>
+    public class NumericBox : TextBox
+    {
+        /// <summary>
+        /// Конструктор класса NumericBox.
+        /// </summary>
+        public NumericBox()
+        {
+            KeyPress += new KeyPressEventHandler(ValidateInput);
+        }
+
+        /// <summary>
+        /// Корректировка ввода данных.
+        /// </summary>
+        /// <param name="sender">Источник события.</param>
+        /// <param name="e">Объект, содержащий данные о событии.</param>
+        public virtual void ValidateInput(object sender, KeyPressEventArgs e)
+        {
+            char symbol = e.KeyChar;
+            if ((symbol == '.' || symbol == ',')
+                && Text.IndexOf(',') != -1)
+            {
+                e.Handled = true;
+                return;
+            }
+
+            if (!char.IsDigit(symbol) && symbol != (char)Keys.Back
+                && symbol != ',' && symbol != '.')
+            {
+                e.Handled = true;
+            }
+
+            if (symbol == '.')
+            {
+                e.KeyChar = ',';
+            }
+        }
+    }
+}
